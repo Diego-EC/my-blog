a624eeca7b783748a65e14c97b675467e4526025
Diego <diegoezquerro@gmail.com>
2021-02-09 22:49:24 +0100

    apps, models and migrations

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
     path('about/', views.about),
     path('', views.home_page),
 ]
+ 
