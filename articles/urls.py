1f577cb72cd4fa5a038e877836177bc7a23419ed
Diego <diegoezquerro@gmail.com>
2021-02-11 22:52:04 +0100

    URL Parameters, Uploading Media

--- articles/urls.py
+++ articles/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
+    path('<slug:slug>/', views.article_detail, name="detail"),
 ]
